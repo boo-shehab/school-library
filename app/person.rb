764b1034d2f0d5214c30f3fa65849a5a47eb4d59
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-01-30 15:37:22 +0300

    setup project

---
+++ app/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(100_000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def can_use_services?
+    (is_of_age || @parent_permission)
+  end
+
+  private
+
+
+  def of_age?
+    @age >= 18
+  end
+end
