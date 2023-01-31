fc3b48b2b4365e04466a2cd282ca973087f15143
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-01-31 13:29:05 +0300

    add nameable

--- app/person.rb
+++ app/person.rb
@@ -1,5 +1,7 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = rand(100_000)
     @age = age
     @name = name
@@ -11,6 +13,10 @@ class Person
 
   def can_use_services?
     (of_age? || @parent_permission)
+  end
+
+  def correct_name
+    @name
   end
 
   private
