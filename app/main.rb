fbb11744409cc28012e8797ebe9318335f24510e
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-02-03 09:17:29 +0300

    add execute permission

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 require_relative 'app'
 require_relative 'book'
-require_relative 'class_room'
+require_relative 'classroom'
 require_relative 'student'
 require_relative 'rental'
 require_relative 'teacher'
