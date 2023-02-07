9dd82e59d43c63de418d5acc24c3546bc1bb6960
Ahmed Mohsen <ahmedmohsen159357@gmail.com>
2023-02-07 15:43:40 +0200

    Update app.rb

--- app/app.rb
+++ app/app.rb
@@ -46,13 +46,13 @@ class App
 
   def main_section
     'Please choose an option by entering a number:
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person
-7 - Exit'
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person
+  7 - Exit'
   end
 
   def add_person
